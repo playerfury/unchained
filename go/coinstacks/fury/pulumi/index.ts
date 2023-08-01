694bbd4015309f315b5c0e2f632aa1abf77b4055
adrian <adrian@fury.black>
2023-08-01 08:06:56 +0530

    commit

---
+++ go/coinstacks/fury/pulumi/index.ts
@@ -0,0 +1,39 @@
+import { readFileSync } from 'fs'
+import { deployCoinstack } from '../../../../pulumi/src/coinstack'
+import { Outputs, CoinServiceArgs, getConfig } from '../../../../pulumi/src'
+
+//https://www.pulumi.com/docs/intro/languages/javascript/#entrypoint
+export = async (): Promise<Outputs> => {
+  const appName = 'unchained'
+  const coinstack = 'fury'
+  const sampleEnv = readFileSync('../../../cmd/fury/sample.env')
+  const { kubeconfig, config, namespace } = await getConfig()
+
+  const coinServiceArgs = config.statefulService?.services?.map((service): CoinServiceArgs => {
+    switch (service.name) {
+      case 'daemon':
+        return {
+          ...service,
+          dataDir: '/root',
+          ports: {
+            'daemon-api': { port: 1317, pathPrefix: '/lcd', stripPathPrefix: true },
+            'daemon-rpc': { port: 26657, pathPrefix: '/rpc', stripPathPrefix: true },
+          },
+          readinessProbe: { periodSeconds: 30, failureThreshold: 10 },
+        }
+      default:
+        throw new Error(`no support for coin service: ${service.name}`)
+    }
+  })
+
+  return deployCoinstack({
+    appName,
+    coinServiceArgs,
+    coinstack,
+    coinstackType: 'go',
+    config,
+    kubeconfig,
+    namespace,
+    sampleEnv,
+  })
+}
